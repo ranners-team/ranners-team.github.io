8f8960ed91fa1c7fa8be2602a75d6cbb266edead
vicholp <vlinerospardo@gmail.com>
2024-09-11 01:24:18 -0300

    chore: replace tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
+
   runtimeConfig: {
     public: {
       gtagId: "G-YL6HE1XPXP"
     }
-  }
+  },
+
+  modules: ["@nuxtjs/tailwindcss"]
 })
